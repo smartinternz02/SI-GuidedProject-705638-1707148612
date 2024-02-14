374a3d7c5ce9ed3b2346ace0cb050c5fa3df0994
Sarvan29 <143703328+Sarvan29@users.noreply.github.com>
2024-02-15 00:14:13 +0530

    Add files via upload

---
+++ CURA_Project/Scripts/TC_CURA_History_007/Script1707928863883.groovy
@@ -0,0 +1,54 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.selectOptionByLabel(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/select_Tokyo CURA Healthcare Center        _5b4107'), 
+    Facility, true)
+
+WebUI.click(findTestObject('CURA_OR/Page_CURA Healthcare Service/div_input-group-addon'))
+
+WebUI.click(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/th_February 2024'))
+
+WebUI.click(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/th_2024'))
+
+WebUI.click(findTestObject('CURA_OR/Page_CURA Healthcare Service/yearObject', [('year_value') : Year]))
+
+WebUI.click(findTestObject('CURA_OR/Page_CURA Healthcare Service/monthObject', [('month_value') : Month]))
+
+WebUI.click(findTestObject('CURA_OR/Page_CURA Healthcare Service/dateObject', [('date_value') : date]))
+
+WebUI.setText(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/textarea_comment'), Comments)
+
+WebUI.click(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/button_Book Appointment'))
+
+WebUI.verifyElementText(findTestObject('CURA_OR/Page_CURA Healthcare Service/h2_Appointment Confirmation'), 'Appointment Confirmation')
+
+WebUI.click(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/amenu-toggle'))
+
+WebUI.click(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/a_History'))
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/div_27022024'), 0)
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/div_Facility                               _86869a'), 
+    0)
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/a_Go to Homepage'), 0)
+
+WebUI.verifyElementNotPresent(findTestObject('Object Repository/CURA_OR/Page_CURA Healthcare Service/p_No appointment'), 
+    0)
+
